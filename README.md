6ce66cb4210cffc5b9361e4b45f93338872d0515
ds1sqe <ds1sqe@mensakorea.org>
2022-11-06 21:28:50 +0900

    add some Infomation

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-# cusorline Highlighter
-## highlight your cursorline by current mode
+# Cursor line Highlighter
 
-readme wip
+## highlight your "Cursor line" by current mode
+
+Change Line's color by current mode. And if you set true to hide highlight when
+you leave current buffer, remove them
+
+Using Example
+
+Normal Mode
+![Normal](./img/normal.png)
+Insert Mode
+![Insert](./img/insert.png)
+Visual
+![Viual](./img/visual.png)
+
+If you using packer to manage plugins, the below is config example
+
+```lua
+
+use({
+  "ds1sqe/cusorlineHighlighter.nvim",
+  event = "BufReadPost",
+  config = function()
+    require("cusorHl").setup({
+      local config = {
+      normal = "guibg=#355666", -- cusorline color of normal mode
+      insert = "guibg=#2e5c3f", --               "" of insert mode
+      visual = "guibg=#ad51a7", -- selected line's color of visual mode
+      nohighlightIfBufferLeave = true, -- hide highlight if leave current buffer
+	}
+      })
+  end,
+})
+
+
+```
--- lua/cusorHl.lua
+++ lua/cusorHl.lua
@@ -6,68 +6,55 @@ local M = {}
 
 M.toggleWindowHighlight = vim.api.nvim_create_augroup("toggleWindowHighlight", { clear = true })
 M.cusorHighlight = vim.api.nvim_create_augroup("cusorHighlight", { clear = true })
+--  Example of use
+-- require('cusorHl').setup({
+--  normal = "guibg = #355666", -- cusorline color of normal mode
+-- 	insert = "guibg=#2e5c3f", -- "" of insert mode
+-- 	visual = "guibg=#ad51a7", -- selected line's color of visual mode
+-- 	nohighlightIfBufferLeave = true, -- if you leave current buffer, remove highlight
+-- })
+function M.setup(setting)
+	local autoCmd = vim.api.nvim_create_autocmd
 
--- Setup function. you can use this at your main config,
--- require('colorHl').setup("","","",T/F)
--- @param normalModColor => normal mod's cusorline color,
--- ex)
--- @param insertModColor => normal mod's cusorline color,
--- ex)
--- @param visualModColor => normal mod's cusorline color,
--- ex)
--- @param nohighlightIfBufferLeave => True or False ex) "true"
--- ex)
--- default settings are, normal = skyblue, insert = green , visual = pink , turn off if leave buffer = true
-function M.setup(normalModColor, insertModColor, visualModColor, nohighlightIfBufferLeave)
+	-- default settings are, normal = skyblue, insert = green , visual = pink , turn off if leave buffer = true
+	local config = {
+		normal = "guibg=#355666",
+		insert = "guibg=#2e5c3f",
+		visual = "guibg=#ad51a7",
+		nohighlightIfBufferLeave = true,
+	}
 
-    local settings = {
-        normal = "guibg=#355666",
-        insert = "guibg=#2e5c3f",
-        visual = "guibg=#ad51a7",
-        nohighlightIfBufferLeave = true,
-    }
-    if ((normalModColor ~= "") and (normalModColor ~= nil)) then
-        settings.normal = normalModColor
-    end
-    if ((insertModColor ~= "") and (insertModColor ~= nil)) then
-        settings.insert = insertModColor
-    end
-    if ((visualModColor ~= "") and (visualModColor ~= nil)) then
-        settings.visual = visualModColor
-    end
-    if ((nohighlightIfBufferLeave ~= nil) and (type(nohighlightIfBufferLeave) == "boolean")) then
-        settings.nohighlightIfBufferLeave = nohighlightIfBufferLeave
-    end
-    vim.api.nvim_create_autocmd("InsertLeave", {
-        group = M.cusorHighlight,
-        pattern = { "*" },
-        command = "hi CursorLine " .. settings.normal
-    })
-    vim.api.nvim_create_autocmd("InsertEnter", {
-        group = M.cusorHighlight,
-        pattern = { "*" },
-        command = "hi CursorLine " .. settings.insert
-    })
-    vim.api.nvim_create_autocmd("InsertLeave", {
-        group = M.cusorHighlight,
-        pattern = { "*" },
-        command = "hi Visual " .. settings.visual
-    })
+	vim.tbl_extend("force", config, setting)
 
-    if (settings.nohighlightIfBufferLeave) then
-        vim.api.nvim_create_autocmd("BufEnter", {
-            group = M.toggleWindowHighlight,
-            pattern = "*",
-            command = "set relativenumber cursorline",
-        })
+	autoCmd("InsertLeave", {
+		group = M.cusorHighlight,
+		pattern = { "*" },
+		command = "hi CursorLine " .. config.normal,
+	})
+	autoCmd("InsertEnter", {
+		group = M.cusorHighlight,
+		pattern = { "*" },
+		command = "hi CursorLine " .. config.insert,
+	})
+	autoCmd("InsertLeave", {
+		group = M.cusorHighlight,
+		pattern = { "*" },
+		command = "hi Visual " .. config.visual,
+	})
 
-        vim.api.nvim_create_autocmd("BufLeave", {
-            group = M.toggleWindowHighlight,
-            pattern = "*",
-            command = "set norelativenumber nocursorline",
-        })
-    end
+	if config.nohighlightIfBufferLeave then
+		autoCmd("BufEnter", {
+			group = M.toggleWindowHighlight,
+			pattern = "*",
+			command = "set relativenumber cursorline",
+		})
 
+		autoCmd("BufLeave", {
+			group = M.toggleWindowHighlight,
+			pattern = "*",
+			command = "set norelativenumber nocursorline",
+		})
+	end
 end
 
 return M
